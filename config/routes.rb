94dceb942d819c8669699b15c9092f4f78f2fdcd
ronakbansal <ronakbansal@gmail.com>
2012-02-22 13:35:34 +0530

    changed to old one

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Amit::Application.routes.draw do
   devise_for :admins, :path_names => { :sign_in => 'login', :sign_out => 'logout'}
   
-  devise_scope :admins do 
+  devise_scope :admin do 
     get "login", :to => "devise/sessions#new"
     get "logout", :to => "devise/sessions#destroy"
   end 
