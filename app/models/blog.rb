570a86712a3b3e14d17350bf62a3873a928007c5
Manivannan siva <manisiva19@gmail.com>
2012-02-23 13:17:50 +0530

    devise bug  and application crash has been fixed

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -10,21 +10,22 @@ class Blog < ActiveRecord::Base
   #This method will read the posts of the blogs and stores it in DB. Currently we are showing the posts directly from the DB.
   def read_posts(blog)
     @doc = Nokogiri::HTML(open("#{blog.blog_url}"))
-      if !@doc.nil?
-        @doc.css('div.post').each do |node|
-          @post = Post.new
-          title_url = node.css('h3.post-title a')
-          @post.blog_id = blog.id
-          blog.blog_title = @doc.css('h1.title').text
-          blog.save!
-          @post.url = title_url[0]['href']
-          @post.title = node.css('h3.post-title').text          
-          @post.content = node.css('div.post-body').inner_html.chomp
-          @post.author = node.css('span.fn').text          
-          published_date = node.css('abbr.published') 
-          @post.post_date = Time.parse(published_date[0]['title'].gsub('T'," "))          
-          @post.save!
-        end         
+      if !@doc.nil?      
+           @doc.css('div.post').each do |node|
+            @post = Post.new
+            title_url = node.css('h3.post-title a')
+            @post.blog_id = blog.id
+            blog.blog_title = @doc.css('h1.title').text
+            blog.save!
+            @post.url = "#"
+            @post.url = title_url[0]['href'] if !title_url.blank?
+            @post.title = node.css('h3.post-title').text          
+            @post.content = node.css('div.post-body').inner_html
+            @post.author = node.css('span.fn').text          
+            published_date = node.css('abbr.published') 
+            @post.post_date = Time.parse(published_date[0]['title'].gsub('T'," "))          
+            @post.save!
+          end 
       end
   end
   
